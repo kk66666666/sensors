1183535677e02ebe2d466beb4c5c0ca570a09eaf
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2023-04-06 16:47:10 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+music.playTone(659, music.beat(BeatFraction.Double))
+music.playTone(587, music.beat(BeatFraction.Double))
+music.playTone(523, music.beat(BeatFraction.Double))
+music.playTone(494, music.beat(BeatFraction.Double))
+music.playTone(440, music.beat(BeatFraction.Double))
+music.playTone(392, music.beat(BeatFraction.Double))
+music.playTone(440, music.beat(BeatFraction.Double))
+music.playTone(494, music.beat(BeatFraction.Double))
+basic.forever(function () {
+	
+})
